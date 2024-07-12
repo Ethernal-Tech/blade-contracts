5e64d5715ddd0a6a7b7c3568fd9603127ea01ea2
Goran Rojovic <goran.rojovic@ethernal.tech>
2024-07-12 12:57:10 +0200

    implement signature verification

--- test/blade/BridgeStorage.test.ts
+++ test/blade/BridgeStorage.test.ts
@@ -3,13 +3,12 @@ import { BigNumber } from "ethers";
 import * as hre from "hardhat";
 import { ethers } from "hardhat";
 import { MerkleTree } from "merkletreejs";
-import { BridgeStorage} from "../../typechain-types";
+import { BridgeStorage } from "../../typechain-types";
 import { alwaysFalseBytecode, alwaysRevertBytecode, alwaysTrueBytecode } from "../constants";
 import { HashZero } from "./accountAbstraction/testutils";
 
 describe("BridgeStorage", () => {
-  let bridgeStorage: BridgeStorage,
-    msgs: any[];
+  let bridgeStorage: BridgeStorage, msgs: any[];
   before(async () => {
     const BridgeStorage = await ethers.getContractFactory("BridgeStorage");
     bridgeStorage = (await BridgeStorage.deploy()) as BridgeStorage;
@@ -26,46 +25,45 @@ describe("BridgeStorage", () => {
 
     msgs = [];
 
-    msgs = [{
+    msgs = [
+      {
         id: 1,
         sourceChainId: 2,
         destinationChainId: 3,
         sender: ethers.constants.AddressZero,
         receiver: ethers.constants.AddressZero,
         payload: ethers.constants.HashZero,
-    },
-     {
+      },
+      {
         id: 2,
         sourceChainId: 2,
         destinationChainId: 3,
         sender: ethers.constants.AddressZero,
         receiver: ethers.constants.AddressZero,
         payload: ethers.constants.HashZero,
-    }
-    ]
+      },
+    ];
 
-    let sign: [number,number]
+    let sign: [number, number];
 
-    sign = [1,1]
-
+    sign = [1, 1];
 
     const batch = {
-        messages: msgs, 
-        signature:sign,
-        bitmap: ethers.constants.AddressZero
-
-    }
+      messages: msgs,
+      signature: sign,
+      bitmap: ethers.constants.AddressZero,
+    };
 
     const firstTx = await bridgeStorage.commitBatch(batch);
-    const firstReceipt = await firstTx.wait()
+    const firstReceipt = await firstTx.wait();
     const firstLogs = firstReceipt?.events?.filter((log) => log.event === "NewBatch") as any[];
     expect(firstLogs).to.exist;
-    expect(firstLogs[0]?.args?.id).to.equal(0)
+    expect(firstLogs[0]?.args?.id).to.equal(0);
 
     const secondTx = await bridgeStorage.commitBatch(batch);
-    const secondReceipt = await secondTx.wait()
+    const secondReceipt = await secondTx.wait();
     const secondLogs = secondReceipt?.events?.filter((log) => log.event === "NewBatch") as any[];
     expect(secondLogs).to.exist;
     expect(secondLogs[0]?.args?.id).to.equal(1);
-    });
+  });
 });
