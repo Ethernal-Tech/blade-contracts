37185eff9df8e78a0f19b0b509008628d1db2f91
Stefan Negovanović <93934272+Stefan-Ethernal@users.noreply.github.com>
2024-09-10 13:19:18 +0200

    Remove unused VALIDATOR_PKCHECK_PRECOMPILE address (#35)

--- contracts/blade/System.sol
+++ contracts/blade/System.sol
@@ -7,7 +7,6 @@ contract System {
     // pre-compiled contracts
     // slither-disable too-many-digits
     address public constant NATIVE_TRANSFER_PRECOMPILE = 0x0000000000000000000000000000000000002020;
-    address public constant VALIDATOR_PKCHECK_PRECOMPILE = 0x0000000000000000000000000000000000002030;
     address public constant ALLOWLIST_PRECOMPILE = 0x0200000000000000000000000000000000000004;
     address public constant BLOCKLIST_PRECOMPILE = 0x0300000000000000000000000000000000000004;
 
@@ -16,7 +15,6 @@ contract System {
 
     // pre-compiled gas consumption
     uint256 public constant NATIVE_TRANSFER_PRECOMPILE_GAS = 21000;
-    uint256 public constant VALIDATOR_PKCHECK_PRECOMPILE_GAS = 150000;
     uint256 public constant READ_ADDRESSLIST_GAS = 5000;
 
     // genesis contracts
