c8cade6e9aaba108fdf18a8147f50c6cbd3a1e0a
Goran Rojovic <goran.rojovic@ethernal.tech>
2024-10-09 12:12:28 +0200

    fix

--- contracts/blade/BridgeStorage.sol
+++ contracts/blade/BridgeStorage.sol
@@ -73,11 +73,12 @@ contract BridgeStorage is ValidatorSetStorage {
      */
     function _verifyBatch(SignedBridgeMessageBatch calldata batch) private {
         require(batch.rootHash == bytes32(0), "EMPTY_BATCH");
-        require(lastCommitted[batch.sourceChainId] + 1 == batch.startId, "INVALID_LAST_COMMITTED");
 
         if (batch.sourceChainId == block.chainid) {
+            require(lastCommittedInternal[batch.destinationChainId] + 1 == batch.startId, "INVALID_LAST_COMMITTED");
             lastCommittedInternal[batch.destinationChainId] = batch.endId;
         } else {
+            require(lastCommitted[batch.sourceChainId] + 1 == batch.startId, "INVALID_LAST_COMMITTED");
             lastCommitted[batch.sourceChainId] = batch.endId;
         }
     }
