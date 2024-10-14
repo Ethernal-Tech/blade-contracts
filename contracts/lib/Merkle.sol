544e840728b40fd4f37552b40a5ef644bf8b002f
Goran Rojovic <100121253+goran-ethernal@users.noreply.github.com>
2024-10-14 15:54:30 +0200

    Merkle Tree root hash calculation fix (#42)
    * fix and tests
    
    * missing new line

--- contracts/lib/Merkle.sol
+++ contracts/lib/Merkle.sol
@@ -20,13 +20,12 @@ library Merkle {
             uint256 nextLevelSize = (leaves.length + 1) / 2;
             bytes32[] memory nextLevel = new bytes32[](nextLevelSize);
 
-            for (uint256 i = 0; i < leaves.length / 2; i++) {
-                nextLevel[i] = keccak256(abi.encodePacked(leaves[2 * i], leaves[2 * i + 1]));
-            }
-
-            // If the number of leaves is odd, carry forward the last leaf
-            if (leaves.length % 2 == 1) {
-                nextLevel[nextLevel.length - 1] = leaves[leaves.length - 1];
+            uint256 j = 0;
+            for (uint256 i = 0; i < leaves.length; i += 2) {
+                bytes32 left = leaves[i];
+                bytes32 right = i + 1 < leaves.length ? leaves[i + 1] : left;
+                nextLevel[j] = keccak256(abi.encode(left, right));
+                j++;
             }
 
             leaves = nextLevel;
