de28a0459a0c218b5696a452b5751db2b19790f6
Dušan Nosović <118283942+dusannosovic-ethernal@users.noreply.github.com>
2024-12-11 14:22:22 +0100

    Threshold check on gateway (#48)
    * check for threshold
    
    * updated revert message for threshold
    
    * fix for revert

--- contracts/blade/Gateway.sol
+++ contracts/blade/Gateway.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import "./ValidatorSetStorage.sol";
 import "../interfaces/IGateway.sol";
 import "../lib/Merkle.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Gateway is ValidatorSetStorage, IGateway {
     uint256 public constant MAX_LENGTH = 2048;
@@ -91,6 +92,10 @@ contract Gateway is ValidatorSetStorage, IGateway {
         );
 
         verifySignature(bls.hashToPoint(DOMAIN_BRIDGE, hash), signedBridgeBatch.signature, signedBridgeBatch.bitmap);
+
+        if (block.number > signedBridgeBatch.threshold && !signedBridgeBatch.isRollback) {
+            revert("the batch has timed out");
+        }
 
         uint256 length = batchMessages.length;
         if (!signedBridgeBatch.isRollback) {
