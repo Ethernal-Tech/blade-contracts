9d3c3589690093bac16d1f8f0a462496e6fe5292
Dusan Nosovic <dusan.nosovic@ethernal.tech>
2025-01-23 12:11:25 +0100

    test fix

--- test/forge/blade/Gateway.t.sol
+++ test/forge/blade/Gateway.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import "@utils/Test.sol";
 import {Gateway} from "contracts/blade/Gateway.sol";
-import {Validator, BridgeMessage, BridgeMessageBatch, DOMAIN_BRIDGE} from "contracts/interfaces/blade/IValidatorSetStorage.sol";
+import {Validator, BridgeMessage,SignedBridgeMessageBatch, BridgeMessageBatch, DOMAIN_BRIDGE} from "contracts/interfaces/blade/IValidatorSetStorage.sol";
 import {BLS} from "contracts/common/BLS.sol";
 import {BN256G2} from "contracts/common/BN256G2.sol";
 import {System} from "contracts/blade/System.sol";
