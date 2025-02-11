af073cc6596dcb414ae611e4c1a3c2edbe60d1d8
Dusan Nosovic <dusan.nosovic@ethernal.tech>
2025-02-11 15:09:38 +0100

    build fix

--- contracts/blade/TestRollbackGateway.sol
+++ contracts/blade/TestRollbackGateway.sol
@@ -43,7 +43,7 @@ contract TestRollbackGateway is Gateway {
         }
 
         // slither-disable-next-line reentrancy-events
-        emit BridgeBatchResult(
+        emit BridgeBatchProcessed(
             true,
             signedBatch.batch.messages[0].id,
             signedBatch.batch.messages[signedBatch.batch.messages.length].id,
