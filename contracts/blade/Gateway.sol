c13c6fce15cc3fc8771551f4aeeef22c1773bb82
Dusan Nosovic <dusan.nosovic@ethernal.tech>
2025-02-25 12:56:13 +0100

    never used fix

--- contracts/blade/Gateway.sol
+++ contracts/blade/Gateway.sol
@@ -201,8 +201,6 @@ contract Gateway is ValidatorSetStorage, IGateway {
         );
     }
 
-    // slither-disable-end dead-code
-
     function _executeRollbackBridgeMessage(BridgeMessage calldata message) private {
         // slither-disable-next-line calls-loop,low-level-calls,reentrancy-no-eth
         (bool success, bytes memory returnData) = message.receiver.call(
@@ -225,6 +223,8 @@ contract Gateway is ValidatorSetStorage, IGateway {
             returnData
         );
     }
+
+    // slither-disable-start dead-code
 
     // slither-disable-next-line unused-state,naming-convention
     uint256[50] private __gap;
